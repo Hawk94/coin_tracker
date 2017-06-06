db6fe5f16dcb612811731ded06f86b4322042098
Hawk94 <x99tom.miller@gmail.com>
2017-06-06 12:32:05 +0100

    Sorting out gunicorn and the wsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -13,5 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "BaseConfiguration")
 
 application = get_wsgi_application()
