ac2fd248dbee1794ad1370f51b19c03f1625d4cc
Hawk94 <x99tom.miller@gmail.com>
2017-06-06 12:35:39 +0100

    Sorting out gunicorn and the wsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application  # NOQA
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
