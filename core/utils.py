d0be8cc8b9f9e633030ff34829b990e0a6c79bfb
Hawk94 <x99tom.miller@gmail.com>
2017-06-10 13:01:16 +0100

    Making just one command

--- core/utils.py
+++ core/utils.py
@@ -3,18 +3,24 @@ from django.core.files.storage import default_storage
 import json
 
 
-def model_to_json(model):
-    qs = model.objects.all()
-    to_json = {}
-    for instance in qs:
-        to_json.update({instance.date.strftime('%Y-%m-%d'): float(instance.price)})
-    
-    json_file = json.dumps(to_json)
+def model_to_json(models):
+    final_json = {}
+
+    for model in models:
+        qs = model.objects.all()
+        to_json = {}
+
+        for instance in qs:
+            to_json.update({instance.date.strftime('%Y-%m-%d'): float(instance.price)})
+
+        final_json.update({model.__name__: to_json})
+
+    json_file = json.dumps(final_json)
     return json_file
 
 
 def save_to_s3(file_name, body):
-    new_file = default_storage.open(file_name + ".json", 'w')
+    new_file = default_storage.open(file_name, 'w')
     new_file.write(body)
     new_file.close()
-    return default_storage.exists(file_name + ".json")
+    return default_storage.exists(file_name)
--- main/coins/management/commands/save_coins.py
+++ main/coins/management/commands/save_coins.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from main.coins.models import BTC, ETH, LTC
+from main.stocks.models import AAPL, AMZN, FB, GOOG, NFLX, MSFT, FANG, FAMGA
 from core.utils import save_to_s3, model_to_json
 
 import requests
@@ -10,10 +11,10 @@ class Command(BaseCommand):
     help = 'Dumps all coin data to S3'
 
     def handle(self, *args, **options):
-        models = [BTC, ETH, LTC]
+        models = [BTC, ETH, LTC, AAPL, AMZN, FB, GOOG, NFLX, MSFT, FANG, FAMGA]
         
         for model in models:
-            json = model_to_json(model)
-            save_to_s3(file_name=model.__name__, body=json)
+            json = model_to_json(models)
+            save_to_s3(file_name='coinsaver_data.json', body=json)
 
         self.stdout.write(self.style.SUCCESS('Successfully saved coins to S3'))
