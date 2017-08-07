3523ee31d526db5036f6e1236b430a29217dcc68
Hawk94 <x99tom.miller@gmail.com>
2017-08-07 21:48:25 +0100

    Changing how the json is constructed

--- backend/settings.py
+++ backend/settings.py
@@ -130,7 +130,7 @@ class BaseConfiguration(Configuration):
     # Storages
     
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_STORAGE_BUCKET_NAME = 'www.coinsaver.io'
+    AWS_STORAGE_BUCKET_NAME = 'www.dustapp.io'
     
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_S3_HOST = 's3.eu-west-2.amazonaws.com'
--- main/coins/management/commands/save_coins.py
+++ main/coins/management/commands/save_coins.py
@@ -15,6 +15,6 @@ class Command(BaseCommand):
         models = [BTC, ETH, LTC, AAPL, AMZN, FB, GOOG, NFLX, MSFT, SPX, FANG, FAMGA, Rate]
 
         json = model_to_json(models)
-        save_to_s3(file_name='coinsaver_data.json', body=json)
+        save_to_s3(file_name='dustapp_data.json', body=json)
 
         self.stdout.write(self.style.SUCCESS('Successfully saved coins to S3'))
