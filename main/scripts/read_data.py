6662e1572d038fe9710ad84d440bda3067e36f32
Hawk94 <x99tom.miller@gmail.com>
2017-08-09 18:35:56 +0100

    Scripts to clean data

--- main/scripts/read_data.py
+++ main/scripts/read_data.py
@@ -62,3 +62,83 @@ def make_stocks(infile, object):
                     stock = object.objects.create(date=created + datetime.timedelta(days=1), price=stock.price)
             price = Decimal(row[1])
             stock = object.objects.create(date=created, price=price)
+
+def make_FAMGA():
+    start = datetime.datetime.strptime('2013-1-1', "%Y-%M-%d").date()
+    end = datetime.datetime.strptime('2014-12-31', "%Y-%M-%d").date()
+    delta = end - start
+    for i in range(delta.days + 1):
+        date = start + datetime.timedelta(days=i)
+        try:
+            aapl = AAPL.objects.filter(date=date).first().price
+        except:
+            aapl = 0
+        try:
+            amzn = AMZN.objects.filter(date=date).first().price
+        except:
+            amzn = 0
+        try:
+            fb = FB.objects.filter(date=date).first().price
+        except:
+            fb = 0
+        try:
+            goog = GOOG.objects.filter(date=date).first().price
+        except:
+            goog = 0
+        try:
+            msft = MSFT.objects.filter(date=date).first().price
+        except:
+            msft = 0
+        prices = [aapl,
+                  amzn,
+                  fb,
+                  goog,
+                  msft]
+        FAMGA.objects.create(date=date, price=decimal.Decimal(sum(prices)))
+
+def clean_data(object):
+    start = datetime.datetime.strptime('2013-1-1', "%Y-%M-%d").date()
+    end = datetime.datetime.strptime('2017-8-9', "%Y-%M-%d").date()
+    delta = end - start
+    clean_first(object)
+    for i in range(delta.days + 1):
+        date = start + datetime.timedelta(days=i)
+        try:
+            all_objects = object.objects.filter(date=date)
+            len_o = len(all_objects)
+            for i in range(len_o-1):
+                all_objects[i].delete()
+        except:
+            price = object.objects.filter(date=date-datetime.timedelta(days=1)).first().price
+            object.objects.create(date=date, price=price)
+
+def clean_first(object):
+    date = datetime.datetime.strptime('2013-1-1', "%Y-%M-%d").date()
+    all_objects = object.objects.filter(date=date)
+    len_o = len(all_objects)
+    for i in range(len_o-1):
+        all_objects[i].delete()
+
+def clean_rates():
+    start = datetime.datetime.strptime('2013-1-1', "%Y-%M-%d").date()
+    end = datetime.datetime.strptime('2017-8-9', "%Y-%M-%d").date()
+    delta = end - start
+    clean_first_rate()
+    for i in range(delta.days + 1):
+        date = start + datetime.timedelta(days=i)
+        try:
+            all_objects = Rate.objects.filter(date=date)
+            len_o = len(all_objects)
+            for i in range(len_o-1):
+                all_objects[i].delete()
+        except:
+            eur_price = Rate.objects.filter(date=date-datetime.timedelta(days=1)).first().eur_price
+            gbp_price = Rate.objects.filter(date=date-datetime.timedelta(days=1)).first().gbp_price
+            Rate.objects.create(date=date, eur_price=eur_price, gbp_price=gbp_price)
+
+def clean_first_rate():
+    date = datetime.datetime.strptime('2013-1-1', "%Y-%M-%d").date()
+    all_objects = Rate.objects.filter(date=date)
+    len_o = len(all_objects)
+    for i in range(len_o-1):
+        all_objects[i].delete()
